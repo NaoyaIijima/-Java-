8fdd0fdeb0129626e92a2917d686660fa80c51d4
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-20 18:03:10 +0900

    メソッドaddの追加

--- chap5/main.java
+++ chap5/main.java
@@ -2,9 +2,15 @@ public class Main{
     public static void main(String[] args){
         // テーマ：メソッド
         // hello("naoya");
+        add(10, 20); // 豆知識：渡す引数は実引数，受け取る引数を仮引数
     }
     // 名前を表示するメソッド
     public static void hello(String name){
         System.out.println("hello, " + name);
+    }
+    // 引数を複数取る場合
+    public static void add(int n1, int n2){
+        int ans = n1 + n2;
+        System.out.println(ans);
     }
 }
