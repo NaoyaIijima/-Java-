c85c55d82bfdaa158649ecf82d454a539918232a
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-20 18:17:07 +0900

    オーバーロードの利用

--- chap5/main.java
+++ chap5/main.java
@@ -2,12 +2,17 @@ public class Main{
     public static void main(String[] args){
         // テーマ：メソッド
         // hello("naoya");
-        add(10, 20); // 豆知識：渡す引数は実引数，受け取る引数を仮引数
+        // add(10, 20); // 豆知識：渡す引数は実引数，受け取る引数を仮引数という
 
         int i = 0; // ローカル変数
 
-        System.out.println(retA());
+        // System.out.println(retA());
         retA(); // 戻り値を必ずしも変数で受け取る必要はない
+
+        // オーバーロードの利用（仮引数が異なれば，同じメソッド名でも複数の定義が可能）
+        System.out.println(add(10, 20));
+        System.out.println(add(3.5, 2.7));
+        System.out.println(add("hello, ", "world!"));
     }
 
     // 名前を表示するメソッド
@@ -16,16 +21,25 @@ public class Main{
     }
 
     // 引数を複数取る場合
-    public static void add(int n1, int n2){
+    public static int add(int x, int y){
         int i; // mainメソッド内のiとは別の変数
-        int ans = n1 + n2; // ローカル変数
-        System.out.println(ans);
+        // int ans = x + y; // ローカル変数
+        // System.out.println(ans);
+        return x + y;
     }
 
     // 戻り値の利用
     public static String retA(){
         return "naoya!!!";
         // int i = 0; // コンパイルエラー
+    }
+
+    // オーバーロード
+    public static double add(double x, double y){
+        return x + y;
+    }
+    public static String add(String x, String y){
+        return x + y;
     }
 
 }
