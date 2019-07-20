105c295b784f75e97627bb442d4e751d5620f48d
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-20 17:59:45 +0900

    少し変更

--- chap5/main.java
+++ chap5/main.java
@@ -1,9 +1,10 @@
 public class Main{
-    public static void hello(String name){
-        System.out.println("hello, " + name);
-    }
     public static void main(String[] args){
         // テーマ：メソッド
-        hello("naoya");
+        // hello("naoya");
+    }
+    // 名前を表示するメソッド
+    public static void hello(String name){
+        System.out.println("hello, " + name);
     }
 }
