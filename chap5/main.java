746417f1142b2fa1f2881d13950e983526841d13
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-20 17:59:25 +0900

    引数の追加

--- chap5/main.java
+++ chap5/main.java
@@ -1,5 +1,9 @@
 public class Main{
+    public static void hello(String name){
+        System.out.println("hello, " + name);
+    }
     public static void main(String[] args){
-        
+        // テーマ：メソッド
+        hello("naoya");
     }
 }
