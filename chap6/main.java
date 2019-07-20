147efb519d2c0baef33469de02a6b944f8b827db
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-20 19:59:28 +0900

    ソースコードの分割ができた！！！

--- chap6/main.java
+++ chap6/main.java
@@ -1,5 +1,13 @@
+package calcapp.main;
+
+// File name == Class name
 public class Main{
     public static void main(String[] args){
-        
+        int a = 10; int b = 2;
+        int total = calcapp.logics.CalcLogic.tasu(a, b);
+        int delta = calcapp.logics.CalcLogic.hiku(a, b);
+        System.out.println("added: " + total +  ", subbed: " + delta);
+        // CalcLogic.hiku(a, b);
+
     }
 }
