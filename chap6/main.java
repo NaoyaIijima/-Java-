90cef9db220bebb995e84756fd5b6729996f8621
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-21 15:33:45 +0900

    import文について

--- chap6/main.java
+++ chap6/main.java
@@ -1,11 +1,13 @@
 package calcapp.main;
+// add import 
+import calcapp.logics.CalcLogic;
 
 // File name == Class name
 public class Main{
     public static void main(String[] args){
         int a = 10; int b = 2;
-        int total = calcapp.logics.CalcLogic.tasu(a, b);
-        int delta = calcapp.logics.CalcLogic.hiku(a, b);
+        int delta = CalcLogic.hiku(a, b);
+        int total = CalcLogic.tasu(a, b);
         System.out.println("added: " + total +  ", subbed: " + delta);
         // CalcLogic.hiku(a, b);
 
