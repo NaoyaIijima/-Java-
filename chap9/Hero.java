e9a46f09db9e1a86c3b9a3ae16a11f1edf5a08f1
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-22 11:19:43 +0900

    第9章の準備とソードクラスの実装

---
+++ chap9/Hero.java
@@ -0,0 +1,32 @@
+public class Hero{
+
+    String name;
+    int hp = 10;
+    Sword sword;
+
+    void sleep(){
+        this.hp = 100;
+        System.out.println(this.name + " is sleeping and recover Hit Point!!!");
+    }
+
+
+    void sit(int sec){
+        this.hp += sec;
+        System.out.println(this.name + " is sitting within" + sec + "[sec].");
+        System.out.println("HP increase" + sec + "points!!!");
+    }
+
+
+    void slip(){
+        this.hp -= 5;
+        System.out.println(this.name + " is slipping...");
+        System.out.println("5 points damage...");
+    }
+
+
+    void run(){
+        System.out.println(this.name + " is running.");
+        System.out.println("GAMEOVER!!!!!!!!!!!!!!!!");
+        System.out.println("Final HP is " + this.hp);
+    }
+}
