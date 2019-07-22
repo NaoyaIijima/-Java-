c804ee4684c86008c5e82cdb115ce66b412bbc8b
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-22 11:09:28 +0900

    第8章終わり！！！

--- chap8/main.java
+++ chap8/main.java
@@ -6,12 +6,24 @@ public class Main{
         // Set initial values of fields
         h.name = "Minato";
         h.hp = 100;
-        System.out.println("Hero " + h.name + "is generated!");
+        // System.out.println("Hero " + h.name + "is generated!");
+
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
 
         // Call methods
-        h.sit(5);
+        // h.sit(5);
+
+        m1.run();
+        m2.run();
+
         h.slip();
-        h.sit(25);
+        // h.sit(25);
         h.run();
     }
 }
