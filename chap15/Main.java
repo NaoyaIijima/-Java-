e090634b0715f2f7bbe283051f643adef8207f02
naoya iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-07-25 12:15:20 +0900

    importを含むコードのコンパイル

--- chap15/Main.java
+++ chap15/Main.java
@@ -1,14 +1,19 @@
 import java.io.*;
+import java.util.Date;
 
 public class Main{
     public static void main(String[] args){
-        // FileWriter fw = new FileWriter("data.txt"); // コンパイルエラー
-        try{
-            FileWriter fw = new FileWriter("data.txt");
-        }catch(IOException e){
-            System.out.println("An error occured.");
-        }finally{
-            fw.close();
-        }
+        // // FileWriter fw = new FileWriter("data.txt"); // コンパイルエラー
+        // try{
+        //     FileWriter fw = new FileWriter("data.txt");
+        // }catch(IOException e){
+        //     System.out.println("An error occured.");
+        // }finally{ // MUST WRITE WHEN YOU USE FILEIO and NETWORK CONNECTION.
+        //     fw.close();
+        // }
+
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(now.getTime());
     }
 }
